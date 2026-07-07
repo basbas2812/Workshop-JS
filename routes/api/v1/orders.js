1bed5cadc080592fb778c50795299f5b08906ee0
Chainat Lumyos <basbas2812@gmail.com>
2026-07-07 15:38:55 +0700

    fix get all order

--- routes/api/v1/orders.js
+++ routes/api/v1/orders.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-var ROLES = require("../../../constants/roles");
 var STATUS = require("../../../constants/status");
 var Product = require("../../../models/product.model");
 var Order = require("../../../models/order.model");
@@ -16,27 +15,10 @@ router.use(auth);
 // get all orders
 router.get("/", async function (req, res) {
   try {
-    let filter = {};
-
-    if (req.user.role === ROLES.USER) {
-      filter.userId = req.user.id;
-    }
-
-    if (req.user.role === ROLES.SHOP) {
-      const products = await Product.find({
-        createdBy: req.user.id,
-        isActive: true,
-      }).select("_id");
-      filter.productId = { $in: products.map((product) => product._id) };
-    } else {
-      const activeProducts = await Product.find({ isActive: true }).select(
-        "_id",
-      );
-      filter.productId = {
-        ...filter.productId,
-        $in: activeProducts.map((product) => product._id),
-      };
-    }
+    const activeProducts = await Product.find({ isActive: true }).select("_id");
+    const filter = {
+      productId: { $in: activeProducts.map((product) => product._id) },
+    };
 
     const orders = await Order.find(filter)
       .populate("productId", "productName quantity price isActive")
